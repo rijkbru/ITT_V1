6ef228303ea15ef5b17317dc7a11e144646bff8a
Svenja Jungen <svenni-J@web.de>
2018-06-28 11:08:11 +0200

    test

--- app/src/main/java/com/example/rijke/itt_v1/Startseite.java
+++ app/src/main/java/com/example/rijke/itt_v1/Startseite.java
@@ -1,6 +1,7 @@
 package com.example.rijke.itt_v1;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import junit.framework.Test;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class Startseite extends AppCompatActivity {
@@ -24,7 +32,11 @@ public class Startseite extends AppCompatActivity {
         Intent intent = getIntent();
         String Vorname = intent.getStringExtra("Vorname");
 
-        String nachricht = "Hallo " + Vorname + "!";
+        String nachricht = "Hallo " +  Vorname + "!";
         TextViewWelcomeMessage.setText(nachricht);
-    }
-}
+
+        }
+
+
+        }
+
